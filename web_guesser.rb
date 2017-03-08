b2cd88104d2aeb81e2396d95ad7f1bdf2c247eea
richjdsmith <rich@richjdsmith.ca>
2017-03-08 12:49:40 -0800

    I4: Added background color change on win.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,23 +2,36 @@ require "sinatra"
 require "sinatra/reloader"
 
 
-SECRET_NUMBER = rand(100) + 1
+@@secret_number = rand(100) + 1
+message = ""
+@@background_color = "grey"
+
 
 
 def check_guess(guess)
-  if guess.to_i > SECRET_NUMBER
-    message = "That number is too high, try again."
-  elsif guess.to_i < SECRET_NUMBER
-    message = "That number is too low, try again."
+  if guess > @@secret_number
+    if (guess - @@secret_number) > 10
+    message = "You're over 10 off too high, try again."
+    else
+    message = "You're close, but still to high, try again"
+    end
+  elsif guess.to_i < @@secret_number
+    if (@@secret_number - guess) > 10
+      message = "That number is over 10 too low, try again."
+    else
+    message = "You're close, but too low, try again."
+    end
   else
-    message = "You got it! The secret number was #{SECRET_NUMBER}!"
+    @@background_color = "green"
+    message = "Nailed it! #{@@secret_number} was the secret number!"
+    #background_color = "Green"
   end
 end
 
 get '/' do
-  guess = params['guess']
+  guess = params['guess'].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:number => @@secret_number, :message => message, :background_color => @@background_color,}
 
 end
 
